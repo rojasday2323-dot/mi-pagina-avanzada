37b135ea8c754d36564b56a86145caf8e67632f3
Dayana Rojas <rojasday2323@gmail.com>
2025-10-23 10:32:24 -0600

    Cambio: al presionar Cambiar Tema el fondo se vuelve rosa

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const botonTema = document.getElementById('tema-btn');
 const body = document.body;
 
 botonTema.addEventListener('click', () => {
-    body.classList.toggle('dark');
+    body.classList.toggle('rosa');
 
-    if (body.classList.contains('dark')) {
+    if (body.classList.contains('rosa')) {
         botonTema.textContent = 'Cambiar a Claro';
     } else {
         botonTema.textContent = 'Cambiar Tema';
